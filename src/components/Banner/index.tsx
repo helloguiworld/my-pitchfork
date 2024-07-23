3c403471c9fba4ddfffc0f5deb4e32d0aa4b4ed5
Guilherme Feitosa <feitosa.guilhermef@gmail.com>
2024-07-23 05:56:13 -0300

    reduced social media banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -9,14 +9,11 @@ import './styles.scss'
 export default function Banner() {
     return (
         <div className='banner'>
-            <span className='strong'>We've just launched our social media profiles! 📱✨</span>
+            <span className='strong'>Show off your music taste and join the fun! 🔥</span>
+            <span>Share your reviews on social media using <strong>#mypitchfork</strong> 📸</span>
+            {/* <span className='strong'>We've just launched our social media profiles! 📱✨</span>
             <span>Follow us on <a href="https://www.instagram.com/mypitchfork.fun" target="_blank">Instagram</a> and <a href="https://x.com/mypitchforkfun" target="_blank">X</a> for the latest updates, fun content, and more.</span>
-            <span>Share your reviews with <strong>#mypitchfork</strong> - show off your music taste and join the fun! 📸🔥</span>
-            {/* <span>Stay tuned for updates, fun content, and more!</span> */}
-            {/* <div className='line'>
-                <a href="https://www.instagram.com/yourhandle" target="_blank">Follow us on Instagram</a>
-                <a href="https://x.com/yourhandle" target="_blank">Follow us on X</a>
-            </div> */}
+            <span>Share your reviews with <strong>#mypitchfork</strong> - show off your music taste and join the fun! 📸🔥</span> */}
         </div>
     )
 }
