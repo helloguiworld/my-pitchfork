1fdb68187df883db067230c1b4c54cd99a9a1879
Guilherme Feitosa <feitosa.guilhermef@gmail.com>
2024-07-30 16:48:50 -0300

    dynamic local search

--- src/pages/SearchPage/index.tsx
+++ src/pages/SearchPage/index.tsx
@@ -1,4 +1,4 @@
-import { useRef, FormEvent, ChangeEvent } from 'react'
+import { useRef, useEffect, FormEvent, ChangeEvent } from 'react'
 
 import Page from '../../components/Page'
 import Button from '../../components/Button'
@@ -31,6 +31,10 @@ export default function SearchPage() {
         const response = await searchAlbums(searchQ)
         if (response?.status == 200) await clickServices.postSearchClick(searchQ)
     }
+
+    useEffect(() => {
+        if (searchQ && albums.length == 0) searchAlbums(searchQ)
+    }, [])
 
     return (
         <Page id='search-page' banners={['news']}>
