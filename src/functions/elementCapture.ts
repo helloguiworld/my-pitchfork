6f9c4e38ab30fe9096fde0b41583ac23dda985fa
Guilherme Feitosa <feitosa.guilhermef@gmail.com>
2025-09-14 12:52:11 -0300

    change proxy url

--- src/functions/elementCapture.ts
+++ src/functions/elementCapture.ts
@@ -24,7 +24,7 @@ export default function elementCapture(selector: string, fileName?: string, opti
     html2canvas(
         elementToCapture,
         {
-            proxy: `/proxy`,
+            proxy: "http://mypitchfork.fun/proxy/",
             // scrollY: 0,
             ...options
         }
